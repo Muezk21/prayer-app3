6cee7ce21809f22ccac2f0c6828396e1cf6ba6b6
Muezk21 <muezkhan96@gmail.com>
2025-09-07 17:36:38 -0400

    prev fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,10 @@
-// eslint.config.js
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default [
   js.configs.recommended,
